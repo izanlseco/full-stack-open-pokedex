d186d2089e4b532cc8f41e898d313b4b4e267edf
Izan Lopez Seco <IzanLS09@gmail.com>
2021-07-10 10:41:55 +0200

    integration tests

--- cypress/integration/poke.js
+++ cypress/integration/poke.js
@@ -1,10 +1,13 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5000')
+  })
+  it('can open the frontend', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
-  it('pokemon page can be navigated to', function() {
-    cy.visit
+  it('can navigate to a single pokemon page', function() {
+    cy.contains('charmander').click()
+    cy.contains('blaze')
   })
 })
